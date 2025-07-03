2595df17d7919aac37935c9272b0ce131012b4ad
ritika201 <ritika8asharma@gmail.com>
2025-07-04 03:02:36 +0600

    Delete commands/commands directory

--- commands/commands/help.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "help",
-  execute(api, message, args) {
-    api.sendMessage(
-      `🧠 ${args[0] || "Golumulu BOT"} Commands:\n\n/pakhi - Moyna test\n/help - Show this list\n/ai <question> - Ask AI`,
-      message.threadID
-    );
-  }
-};
